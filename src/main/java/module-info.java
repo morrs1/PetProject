669f5ae67e48518ffceadfcff067f7f048bdbc14
morrs <grishkovea@gmail.com>
2024-06-16 23:55:14 +0300

    добавил lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module org.example.petproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
 
     opens org.example.petproject to javafx.fxml;
     exports org.example.petproject;
+    exports org.example.petproject.controllers;
+    opens org.example.petproject.controllers to javafx.fxml;
 }
