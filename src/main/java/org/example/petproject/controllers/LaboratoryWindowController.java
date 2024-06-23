8c7797476423af43f4a95992bdadfffd5ac1da15
morrs <grishkovea@gmail.com>
2024-06-23 16:56:34 +0300

    сделал переключатель сцен(не конечный результат)

--- src/main/java/org/example/petproject/controllers/LaboratoryWindowController.java
+++ src/main/java/org/example/petproject/controllers/LaboratoryWindowController.java
@@ -1,4 +1,14 @@
 package org.example.petproject.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LaboratoryWindowController {
+    @FXML
+    private Button backToMenuButton;
+
+    @FXML
+    private void onBackToMenuButtonClicked(){
+
+    }
 }
