4030e3dd8f458c66a2db19cebb98509388186991
morrs <grishkovea@gmail.com>
2024-06-23 16:54:59 +0300

    добавил парсинг файла со сценами

---
+++ src/main/java/org/example/petproject/core/json/ParserScenesJson.java
@@ -0,0 +1,21 @@
+package org.example.petproject.core.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ParserScenesJson {
+    public static ScenesJson parseScenesJson(String pathToJson){
+        ObjectMapper objectMapper = new ObjectMapper();
+        File json = new File(pathToJson);
+        ScenesJson scenesJson;
+        try {
+            scenesJson = objectMapper.readValue(json, ScenesJson.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return scenesJson;
+    }
+}
