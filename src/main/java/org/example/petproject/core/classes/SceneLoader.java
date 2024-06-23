0c81bc7ea64cbfa8f962ad00c40f77a21bc2e5a4
morrs <grishkovea@gmail.com>
2024-06-23 16:56:02 +0300

    сделал "кэштрование сцен" (теперь адекватно)

--- src/main/java/org/example/petproject/core/classes/SceneLoader.java
+++ src/main/java/org/example/petproject/core/classes/SceneLoader.java
@@ -2,34 +2,38 @@ package org.example.petproject.core.classes;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import lombok.Getter;
 import org.example.petproject.Main;
-import org.example.petproject.core.enums.Scenes;
+import org.example.petproject.core.json.ParserScenesJson;
+import org.example.petproject.core.json.ScenesJson;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 
+@Getter
 public class SceneLoader {
-    public static final Scene mainScene;
-    public static final Scene laboratoryScene;
+    private static SceneLoader instance;
+    private  HashMap<String, Scene> scenes;
 
-    static {
-        try {
-            mainScene = new Scene(new FXMLLoader(Main.class.getResource("mainWindow.fxml")).load());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public static SceneLoader getInstance() {
+        if (instance == null) {
+            instance = new SceneLoader();
+            ScenesJson scenesJson = ParserScenesJson.parseScenesJson("src/main/resources/org/example/petproject/Scenes.json");
+            instance.scenes = new HashMap<>();
+            scenesJson.pathToScenes().forEach((key, value) -> {
+                try {
+                    instance.scenes.put(key, new Scene(new FXMLLoader(Main.class.getResource(value)).load()));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
+        return instance;
     }
 
-    static {
-        try {
-            laboratoryScene = new Scene(new FXMLLoader(Main.class.getResource("laboratoryWindow.fxml")).load());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private SceneLoader(){
+
     }
-
-
 
 }
