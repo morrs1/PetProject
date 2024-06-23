8bf8e8893e8a8c089e3dc9e3cd14ff17fff52678
morrs <grishkovea@gmail.com>
2024-06-23 17:16:05 +0300

    переделал SceneController на синглтон

--- src/main/java/org/example/petproject/core/SceneController.java
+++ src/main/java/org/example/petproject/core/SceneController.java
@@ -1,15 +1,31 @@
 package org.example.petproject.core;
 
 import javafx.stage.Stage;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import org.example.petproject.core.classes.SceneLoader;
 import org.example.petproject.core.enums.Scenes;
 
-@RequiredArgsConstructor
+
 public class SceneController {
-    final Stage stage;
+    @Getter
+    private static SceneController instance;
+     Stage stage;
+
+    public static SceneController getInstance(Stage stage) {
+        if (instance == null) {
+            instance = new SceneController();
+            instance.stage = stage;
+        }
+        return instance;
+    }
+
 
     public void setScene(Scenes scene) {
-        stage.setScene(SceneLoader.getInstance().getScenes().get(scene.getName()));
+        instance.stage.setScene(SceneLoader.getInstance().getScenes().get(scene.getName()));
+    }
+
+
+    private SceneController(){
+
     }
 }
