ef9e1eead403d34bc58d394cdd896aa24e426f85
morrs <grishkovea@gmail.com>
2024-06-29 18:40:42 +0300

    добавил сцену для второго задания

---
+++ src/main/java/org/example/petproject/controllers/laboratory/tasksForLaboratoryControllers/SecondTaskController.java
@@ -0,0 +1,6 @@
+package org.example.petproject.controllers.laboratory.tasksForLaboratoryControllers;
+
+import org.example.petproject.controllers.BaseController;
+
+public class SecondTaskController extends BaseController {
+}
