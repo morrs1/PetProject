d41591fee1d529a8109ac0e8f49feda162432e94
morrs <grishkovea@gmail.com>
2024-06-29 20:00:27 +0300

    переделал логику под один метод

--- src/main/java/org/example/petproject/controllers/laboratory/LaboratoryWindowController.java
+++ src/main/java/org/example/petproject/controllers/laboratory/LaboratoryWindowController.java
@@ -1,17 +1,27 @@
 package org.example.petproject.controllers.laboratory;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import org.example.petproject.controllers.BaseController;
 import org.example.petproject.controllers.SceneController;
 import org.example.petproject.core.enums.Scenes;
+import org.w3c.dom.events.Event;
 
 /**
  * Класс, для управления сценой с лабой
  */
 public class LaboratoryWindowController extends BaseController {
     @FXML
-    protected void onFirstButtonClick(){
-        SceneController.getInstance().setScene(Scenes.FIRST_TASK_WINDOW);
-    }
+    protected void onTaskButtonClick(MouseEvent event){
+        Button pressedButton = (Button) event.getSource();
+        String numberOfTask = pressedButton.getText();
 
+        Scenes sceneToSwitch = switch (numberOfTask){
+            case "1"-> Scenes.FIRST_TASK_WINDOW;
+            default -> throw new IllegalStateException("Unexpected value: " + numberOfTask);
+        };
+        SceneController.getInstance().setScene(sceneToSwitch);
+    }
 }
