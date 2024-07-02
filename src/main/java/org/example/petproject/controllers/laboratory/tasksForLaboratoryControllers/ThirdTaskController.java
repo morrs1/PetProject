72433bb0bff8e646a02b14755334d0ff4e6a971d
morrs <grishkovea@gmail.com>
2024-07-03 02:18:56 +0300

    доделал реализацию круга, пока без сохранения файла

--- src/main/java/org/example/petproject/controllers/laboratory/tasksForLaboratoryControllers/ThirdTaskController.java
+++ src/main/java/org/example/petproject/controllers/laboratory/tasksForLaboratoryControllers/ThirdTaskController.java
@@ -4,11 +4,13 @@ package org.example.petproject.controllers.laboratory.tasksForLaboratoryControll
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Shape;
 import org.example.petproject.controllers.BaseController;
+import org.example.petproject.core.enums.Directions;
 import org.example.petproject.model.secondTask.ModifiedCircleShape;
 import org.example.petproject.model.secondTask.Painter;
 import org.example.petproject.model.secondTask.ShapeFactory;
@@ -32,19 +34,46 @@ public class ThirdTaskController extends BaseController implements Initializable
     ComboBox<String> comboBoxForTypeOfContour;
     @FXML
     AnchorPane paneForThirdTask;
-
+    @FXML
+    TextField heightForImageToSave;
+    @FXML
+    TextField widthForImageToSave;
+    @FXML
+    Button buttonForSave;
+    @FXML
+    Button buttonForHelp;
+    @FXML
+    Button backButton;
+    @FXML
+    Button exitButton;
+    @FXML
+    AnchorPane mainPaneThirdTask;
     Painter painter = new Painter();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        thirdTaskToggleButtonsGroup.getToggles().forEach(toggle -> ((ToggleButton) toggle).setFocusTraversable(false));
+        colorPikerForFill.setFocusTraversable(false);
+        colorPickerForContour.setFocusTraversable(false);
+        textFieldForWidthOfContour.setFocusTraversable(false);
+        comboBoxForTypeOfContour.setFocusTraversable(false);
+        heightForImageToSave.setFocusTraversable(false);
+        widthForImageToSave.setFocusTraversable(false);
+        buttonForSave.setFocusTraversable(false);
+        buttonForHelp.setFocusTraversable(false);
+        backButton.setFocusTraversable(false);
+        exitButton.setFocusTraversable(false);
+        mainPaneThirdTask.setFocusTraversable(true);
         Arrays.asList("Dotted", "Solid").forEach(value -> comboBoxForTypeOfContour.getItems().add(value));
 
     }
 
     @FXML
     protected void onPaneForFigureClick(MouseEvent mouseEvent) {
+        mainPaneThirdTask.requestFocus();
 
-        if (!Objects.equals(textFieldForWidthOfContour.getText(), "")) {
+        if (!Objects.equals(textFieldForWidthOfContour.getText(), "") && !Objects.equals(comboBoxForTypeOfContour.getValue(), null)
+                && thirdTaskToggleButtonsGroup.getSelectedToggle() != null) {
 
             ModifiedCircleShape newShape = (ModifiedCircleShape) ShapeFactory.create((Shape)
                             (((ToggleButton) thirdTaskToggleButtonsGroup.getSelectedToggle()).getChildrenUnmodifiable().getFirst()),
@@ -61,6 +90,38 @@ public class ThirdTaskController extends BaseController implements Initializable
             }
             painter.setCurrentShape(newShape);
             paneForThirdTask.getChildren().add(newShape.getShape());
+        }
+    }
+
+    @FXML
+    protected void onKeyPressed(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case UP, DOWN, LEFT, RIGHT -> onArrowsPressed(keyEvent);
+            case EQUALS, MINUS -> onPlusMinusPressed(keyEvent);
+            case COMMA, PERIOD -> onLessGreaterPressed(keyEvent);
+        }
+    }
+
+    private void onArrowsPressed(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case UP -> painter.getCurrentShape().moveShapeAndFrame(2, 2, Directions.UP);
+            case DOWN -> painter.getCurrentShape().moveShapeAndFrame(2, 2, Directions.DOWN);
+            case LEFT -> painter.getCurrentShape().moveShapeAndFrame(2, 2, Directions.LEFT);
+            case RIGHT -> painter.getCurrentShape().moveShapeAndFrame(2, 2, Directions.RIGHT);
+        }
+    }
+
+    private void onPlusMinusPressed(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case EQUALS -> painter.getCurrentShape().changeSize(Directions.UP);
+            case MINUS -> painter.getCurrentShape().changeSize(Directions.DOWN);
+        }
+    }
+
+    private void onLessGreaterPressed(KeyEvent keyEvent) {
+        switch (keyEvent.getCode()) {
+            case COMMA -> painter.getCurrentShape().changeSize(Directions.LEFT);
+            case PERIOD -> painter.getCurrentShape().changeSize(Directions.RIGHT);
         }
     }
 }
--- src/main/java/org/example/petproject/model/secondTask/ModifiedCircleShape.java
+++ src/main/java/org/example/petproject/model/secondTask/ModifiedCircleShape.java
@@ -1,29 +1,13 @@
 package org.example.petproject.model.secondTask;
 
 import javafx.scene.shape.Ellipse;
-import javafx.scene.shape.Rectangle;
 import org.example.petproject.model.strategyShapes.CircleShape;
 
 public class ModifiedCircleShape extends CircleShape {
-    private ModifiedCircleShape(Ellipse circle, Integer radius) {
-        super(circle, radius);
-    }
 
     public ModifiedCircleShape(Ellipse circle) {
         super(circle);
+        setupFrame();
     }
-    @Override
-    protected void setupFrame() {
-        Rectangle currentFrame = new Rectangle(
-                circle.getCenterX() - circle.getRadiusX() - 2,
-                circle.getCenterY() - circle.getRadiusY() - 2,
-                2 * circle.getRadiusX() + 4,
-                2 * circle.getRadiusY() + 4
-        );
-
-        currentFrame = setupRectangle(currentFrame);
-        setFrame(currentFrame);
-    }
-
 
 }
