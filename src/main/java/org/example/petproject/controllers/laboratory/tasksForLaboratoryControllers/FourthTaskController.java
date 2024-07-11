b76bc4fe644eb3cf1e9465b7b32c2413ef2a1772
morrs <grishkovea@gmail.com>
2024-07-12 00:37:06 +0300

    примерно разобрался, как работать с графиками

--- src/main/java/org/example/petproject/controllers/laboratory/tasksForLaboratoryControllers/FourthTaskController.java
+++ src/main/java/org/example/petproject/controllers/laboratory/tasksForLaboratoryControllers/FourthTaskController.java
@@ -1,7 +1,35 @@
 package org.example.petproject.controllers.laboratory.tasksForLaboratoryControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import org.example.petproject.controllers.BaseController;
 
 public class FourthTaskController extends BaseController {
+    @FXML
+    LineChart<Number, Number> chartForFunctions;
 
+    @FXML
+    protected void onShowButtonClick() {
+        chartForFunctions.setLegendVisible(false);
+        XYChart.Series<Number, Number> sinSeries = new XYChart.Series<>();
+
+// Заполняем Series данными
+        for (double x = -0.2 * Math.PI; x <= 0.2 * Math.PI; x += 0.1) {
+            sinSeries.getData().add(new XYChart.Data<>(x, Math.sin(x)));
+        }
+
+// Добавляем Series в LineChart
+
+        chartForFunctions.getData().add(sinSeries);
+        for (XYChart.Data<Number, Number> data : sinSeries.getData()) {
+            Node point = data.getNode();
+            point.setStyle(
+                    "-fx-background-color: blue;"
+            );
+        }
+        chartForFunctions.lookup(".series0").setStyle("-fx-stroke-width: 2px;" + "-fx-stroke: blue;");
+
+    }
 }
