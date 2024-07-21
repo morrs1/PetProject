44c3cc3d14af72ebcd3e022b14fc64fdf2b472dd
morrs <grishkovea@gmail.com>
2024-07-21 13:07:11 +0300

    сделал парсер для xyz файлов

--- src/main/java/org/example/petproject/core/xyz/ParserXYZ.java
+++ src/main/java/org/example/petproject/core/xyz/ParserXYZ.java
@@ -11,21 +11,30 @@ import java.util.stream.IntStream;
 
 public class ParserXYZ {
     public static MoleculeXYZ parseXYZ(String pathToXYZ) {
-        ArrayList<String> lines;
-        try {
-            lines = (ArrayList<String>) Files.readAllLines(Path.of(pathToXYZ));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(lines);
-        LinkedHashMap<String, ArrayList<Double>> descriptionOfAtoms = new LinkedHashMap<>();
+        ArrayList<String> lines = readAllLinesOfFile(pathToXYZ);
+        LinkedHashMap<String, ArrayList<ArrayList<Double>>> descriptionOfAtoms = new LinkedHashMap<>();
         IntStream.range(2, lines.size()).forEach(indexOfRow -> {
-            String typeOfAtom = lines.get(indexOfRow).substring(0,1);
+            String typeOfAtom = lines.get(indexOfRow).substring(0, 1);
             List<String> coordinatesAsString = Arrays.asList(lines.get(indexOfRow).substring(2).split(" "));
             ArrayList<Double> coordinates = new ArrayList<>();
             coordinatesAsString.forEach(coordinate -> coordinates.add(Double.parseDouble(coordinate)));
-            descriptionOfAtoms.put(typeOfAtom, coordinates);
+            if (!descriptionOfAtoms.containsKey(typeOfAtom)) {
+                var list = new ArrayList<ArrayList<Double>>();
+                list.add(coordinates);
+                descriptionOfAtoms.put(typeOfAtom, list);
+            } else {
+                descriptionOfAtoms.get(typeOfAtom).add(coordinates);
+            }
+
         });
         return new MoleculeXYZ(Integer.parseInt(lines.getFirst()), lines.get(1), descriptionOfAtoms);
+    }
+
+    private static ArrayList<String> readAllLinesOfFile(String pathToXYZ){
+        try {
+            return  (ArrayList<String>) Files.readAllLines(Path.of(pathToXYZ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
