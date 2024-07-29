38231d6f8ad00b2bc9deb142df922d1c0919c3e9
morrss <grishkovea@gmail.com>
2024-07-29 21:54:38 +0300

    добавил для всех заданий первой лабы выход через escape

--- src/main/java/org/example/petproject/controllers/laboratory/LaboratoryWindowController.java
+++ src/main/java/org/example/petproject/controllers/laboratory/LaboratoryWindowController.java
@@ -2,11 +2,11 @@ package org.example.petproject.controllers.laboratory;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
 import org.example.petproject.controllers.BaseController;
 import org.example.petproject.controllers.SceneController;
 import org.example.petproject.core.enums.Scenes;
-
 
 
 /**
@@ -14,21 +14,24 @@ import org.example.petproject.core.enums.Scenes;
  */
 public class LaboratoryWindowController extends BaseController {
     @FXML
-    protected void onTaskButtonClick(MouseEvent event){
+    protected void onTaskButtonClick(MouseEvent event) {
         Button pressedButton = (Button) event.getSource();
         String numberOfTask = pressedButton.getText();
 
-        Scenes sceneToSwitch = switch (numberOfTask){
-            case "1"-> Scenes.FIRST_TASK_WINDOW;
-            case "2"-> Scenes.SECOND_TASK_WINDOW;
-            case "3"-> Scenes.THIRD_TASK_WINDOW;
-            case "4"-> Scenes.FOURTH_TASK_WINDOW;
+        Scenes sceneToSwitch = switch (numberOfTask) {
+            case "1" -> Scenes.FIRST_TASK_WINDOW;
+            case "2" -> Scenes.SECOND_TASK_WINDOW;
+            case "3" -> Scenes.THIRD_TASK_WINDOW;
+            case "4" -> Scenes.FOURTH_TASK_WINDOW;
             case "5" -> Scenes.FIFTH_TASK_WINDOW;
             case "6" -> Scenes.SIXTH_TASK_WINDOW;
             case "7" -> Scenes.SEVENTH_TASK_WINDOW;
             default -> throw new IllegalStateException("Unexpected value: " + numberOfTask);
         };
         SceneController.getInstance().setScene(sceneToSwitch);
+        SceneController.getInstance().getStage().getScene().setOnKeyPressed(keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.ESCAPE) SceneController.getInstance().setPreviousScene();
+        });
     }
 
 }
