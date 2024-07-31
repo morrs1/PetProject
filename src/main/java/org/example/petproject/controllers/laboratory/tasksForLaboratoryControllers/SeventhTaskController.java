d89173e11c306f36f6435463326f702c9e9fc7bd
morrss <grishkovea@gmail.com>
2024-07-31 14:26:29 +0300

    доделал 7 задание, но есть момент с отображением видео, иногда не с первого раза показывается

--- src/main/java/org/example/petproject/controllers/laboratory/tasksForLaboratoryControllers/SeventhTaskController.java
+++ src/main/java/org/example/petproject/controllers/laboratory/tasksForLaboratoryControllers/SeventhTaskController.java
@@ -1,20 +1,22 @@
 package org.example.petproject.controllers.laboratory.tasksForLaboratoryControllers;
 
 import javafx.animation.AnimationTimer;
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
 import org.example.petproject.controllers.BaseController;
 
 import java.io.File;
 import java.net.URL;
 
 import javafx.util.Duration;
+import org.example.petproject.controllers.SceneController;
 
 import java.util.ResourceBundle;
 
@@ -25,35 +27,19 @@ public class SeventhTaskController extends BaseController implements Initializab
     Slider sliderForVolume;
     @FXML
     Label labelForVideoDuration;
-    private final String pathToVideo = "src/main/resources/org/example/petproject/Videos/firstVideo.mp4";
+    @FXML
+    Button chooseFileButton;
+    private FileChooser fileChooser;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Platform.runLater(() -> {
-            File file = new File(pathToVideo);
-            Media media = new Media(file.toURI().toString());
-            MediaPlayer mediaPlayer = new MediaPlayer(media);
-            mediaPlayer.setVolume(0.5);
-            mediaViewForSeventhTask.setMediaPlayer(mediaPlayer);
-            mediaPlayer.setOnReady(() ->
-                    labelForVideoDuration.setText("--:-- / " + formatDuration(media.getDuration())));
-
-            AnimationTimer timer = new AnimationTimer() {
-                @Override
-                public void handle(long now) {
-                    labelForVideoDuration.setText(
-                            formatDuration(mediaViewForSeventhTask.getMediaPlayer().getCurrentTime()) +
-                                    " / " +
-                                    formatDuration(media.getDuration())
-                    );
-                }
-            };
-            timer.start();
-        });
+        fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Video Files", "*.mp4"));
+        fileChooser.setInitialDirectory(
+                new File("C:\\Users\\grish\\IdeaProjects\\PetProject\\src\\main\\resources\\org\\example\\petproject\\Videos")
+        );
         sliderForVolume.setValue(50);
         onSliderChangeValue();
-
-
     }
 
     @FXML
@@ -66,6 +52,20 @@ public class SeventhTaskController extends BaseController implements Initializab
         mediaViewForSeventhTask.getMediaPlayer().pause();
     }
 
+    @FXML
+    protected void onChooseFileButtonClicked() {
+        File videoFile = fileChooser.showOpenDialog(SceneController.getInstance().getStage());
+        Media media = new Media(videoFile.toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setVolume(0.5);
+        mediaViewForSeventhTask.setMediaPlayer(mediaPlayer);
+        mediaPlayer.setOnReady(() ->
+                labelForVideoDuration.setText("--:-- / " + formatDuration(media.getDuration()))
+        );
+        setupTimer(media);
+        chooseFileButton.setText(videoFile.getName());
+    }
+
     private void onSliderChangeValue() {
         sliderForVolume.valueProperty().addListener(
                 (observable, oldValue, newValue) ->
@@ -75,8 +75,22 @@ public class SeventhTaskController extends BaseController implements Initializab
 
     private String formatDuration(Duration duration) {
         long minutes = (long) duration.toMinutes();
-        long seconds = (long) duration.toSeconds() % 60; // Остаток от деления на 60 для получения секунд
+        long seconds = (long) duration.toSeconds() % 60;
 
-        return String.format("%02d:%02d", minutes, seconds); // Форматирование в "мин:сек"
+        return String.format("%02d:%02d", minutes, seconds);
+    }
+
+    private void setupTimer(Media media) {
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                labelForVideoDuration.setText(
+                        formatDuration(mediaViewForSeventhTask.getMediaPlayer().getCurrentTime()) +
+                                " / " +
+                                formatDuration(media.getDuration())
+                );
+            }
+        };
+        timer.start();
     }
 }
